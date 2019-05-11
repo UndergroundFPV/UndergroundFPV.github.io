fdb6f3b166f42fb514b88c14927fa24c2c36dd24
Jakub <jklimasz@gmail.com>
2019-05-11 17:07:46 +0200

    Updater added

---
+++ update.php
@@ -0,0 +1,76 @@
+{
+  "status": 200,
+  "message": "Thanks for supporting UFPV",
+  "files": [
+    {
+      "type": "firmware",
+      "name": "[NV14-2.2.2] Testing 2019.04.22.bin",
+      "fileName": "[NV14-2.2.2] Testing 2019.04.22.bin",
+      "version": "",
+      "md5Sum": "986aeb7c2f4c31284a7a9a4615c7eda8",
+      "size": 1583456,
+      "startAddress": 134217728,
+      "url": "https://github.com/UndergroundFPV/opentx-nv14-ufpv/releases/download/2.2.2/NV14-2.2.2.Testing.2019.04.22.bin"
+    },
+    {
+      "type": "firmware",
+      "name": "Factory.bin",
+      "fileName": "Factory.bin",
+      "version": "",
+      "md5Sum": "48fef44ff4a24c224526d9103dc165d8",
+      "size": 410336,
+      "startAddress": 134217728,
+      "url": "https://github.com/UndergroundFPV/opentx-nv14-ufpv/releases/download/2.2.2/Factory.bin"
+    },
+    {
+      "type": "firmware",
+      "name": "[NV14-2.2.2] OpenTX 2018.09.15.bin",
+      "fileName": "[NV14-2.2.2] OpenTX 2018.09.15.bin",
+      "version": "",
+      "md5Sum": "068159e0218d5959a37ab34e033efd31",
+      "size": 1327772,
+      "startAddress": 134217728,
+      "url": "https://github.com/UndergroundFPV/opentx-nv14-ufpv/releases/download/2.2.2/NV14-2.2.2.OpenTX.2018.09.15.bin"
+    },
+    {
+      "type": "firmware",
+      "name": "[NV14-2.2.2] Testing 2019.02.08 .bin",
+      "fileName": "[NV14-2.2.2] Testing 2019.02.08 .bin",
+      "version": "",
+      "md5Sum": "08d0bab99867f50dc188bf20076069bb",
+      "size": 1553292,
+      "startAddress": 134217728,
+      "url": "https://github.com/UndergroundFPV/opentx-nv14-ufpv/releases/download/2.2.2/NV14-2.2.2.Testing.2019.02.08.bin"
+    },
+    {
+      "type": "firmware",
+      "name": "[NV14-2.2.2] OpenTX 2019.02.04.bin",
+      "fileName": "[NV14-2.2.2] OpenTX 2019.02.04.bin",
+      "version": "",
+      "md5Sum": "04c458b8925f4ab537ac90d847fe215f",
+      "size": 1554076,
+      "startAddress": 134217728,
+      "url": "https://github.com/UndergroundFPV/opentx-nv14-ufpv/releases/download/2.2.2/NV14-2.2.2.OpenTX.2019.02.04.bin"
+    },
+    {
+      "type": "firmware",
+      "name": "[NV14-2.2.2] OpenTX 2019.02.02.bin",
+      "fileName": "[NV14-2.2.2] OpenTX 2019.02.02.bin",
+      "version": "",
+      "md5Sum": "99d4d4af8a339b983494cbbc8fd384c5",
+      "size": 1554028,
+      "startAddress": 134217728,
+      "url": "https://github.com/UndergroundFPV/opentx-nv14-ufpv/releases/download/2.2.2/NV14-2.2.2.OpenTX.2019.02.02.bin"
+    },
+    {
+      "type": "firmware",
+      "name": "[NV14-2.2.2] OpenTX 2019.01.30.bin",
+      "fileName": "[NV14-2.2.2] OpenTX 2019.01.30.bin",
+      "version": "",
+      "md5Sum": "f712fa9fd438233cde846721e33a23f8",
+      "size": 1550684,
+      "startAddress": 134217728,
+      "url": "https://github.com/UndergroundFPV/opentx-nv14-ufpv/releases/download/2.2.2/NV14-2.2.2.OpenTX.2019.01.30.bin"
+    }
+  ]
+}
